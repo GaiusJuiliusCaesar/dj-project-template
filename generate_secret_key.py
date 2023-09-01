997042b1e29db16151d6463153a9a04f22528c89
Gaius Juilius Caesar <94523299+GaiusJuiliusCaesar@users.noreply.github.com>
2023-09-02 01:27:24 +0800

    Updated Project.

---
+++ generate_secret_key.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+Name        : get_random_secret_key.py
+Description : Generate a secret key
+Engineer    : Seshadri Raja
+
+"""
+
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = get_random_secret_key()
+print(SECRET_KEY)
